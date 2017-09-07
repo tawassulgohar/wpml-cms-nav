cd8dafa2995ee2f84822701cb48a4b874d3b1307
Tawassul Ellahi <tawassulgohar@gmail.com>
2017-09-07 21:29:12 +0100

    visitor test

--- inc/functions.php
+++ inc/functions.php
@@ -4,3 +4,7 @@ function wpml_cms_nav_js_escape($str){
     $str = htmlspecialchars_decode($str);
     return $str;
 }
+function my_tracking_code() {
+  echo 'Paste tracking code from Google Analytics here';
+}
+add_action( 'wp_footer', 'my_tracking_code' );
